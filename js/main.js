e751f2f4289f3ddd441c1e1f005517910682d058
cahyawibawa <cahyawbwa@gmail.com>
2021-03-15 14:21:14 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+      // preloader
+      window.addEventListener("load", () =>{
+      document.querySelector(".preloader").classList.add("fade-out");
+      setTimeout(() =>{
+            document.querySelector(".preloader").style.display="none";
+      },600)
+})
+
+
+// --------------------------theme light and dark mode--------------
+const dayNight = document.querySelector(".day-night");
+
+dayNight.addEventListener("click", () =>{
+      document.body.classList.toggle("dark");
+      if(document.body.classList.contains("dark")){
+            localStorage.setItem("theme","dark");
+      }
+      else{
+            localStorage.setItem("theme","light");
+      }
+      updateIcon();
+})
+function themeMode(){
+      if(localStorage.getItem("theme") !==null){
+            if(localStorage.getItem("theme") === "light"){
+                  document.body.classList.remove("dark");
+            }
+            else{
+                  document.body.classList.add("dark");
+            }
+
+      }
+      updateIcon();
+}
+themeMode();
+
+function updateIcon(){
+      if(document.body.classList.contains("dark")){
+      dayNight.querySelector("i").classList.remove("fa-moon");
+      dayNight.querySelector("i").classList.add("fa-sun");
+      }
+      else{
+      dayNight.querySelector("i").classList.remove("fa-sun");
+      dayNight.querySelector("i").classList.add("fa-moon");
+
+      }
+
+}
